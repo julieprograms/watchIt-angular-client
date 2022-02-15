843f144d6789b36c2c24c3737c2c4e729b736876
julieprograms <junge.julie@web.de>
2022-02-15 03:07:55 +0100

    added movie card, director-view,genre-view,description-view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,14 +14,35 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
+import { GenreViewComponent } from './genre-view/genre-view.component';
+import { DirectorViewComponent } from './director-view/director-view.component';
+import { DescriptionViewComponent } from './description-view/description-view.component';
+
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
-    UserLoginFormComponent
+    UserLoginFormComponent,
+    MovieCardComponent,
+    WelcomePageComponent,
+    ProfileViewComponent,
+    GenreViewComponent,
+    DirectorViewComponent,
+    DescriptionViewComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +55,9 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    RouterModule.forRoot(appRoutes),
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,110 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
+import { DescriptionViewComponent } from '../description-view/description-view.component';
+import { DirectorViewComponent } from '../director-view/director-view.component';
+import { GenreViewComponent } from '../genre-view/genre-view.component';
+
+
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss']
+})
+
+export class MovieCardComponent implements OnInit {
+
+ // empty states that gets populated in functions
+  movies: any[] = [];
+  favorited: any[] = [];
+
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar
+  ) { }
+
+
+  ngOnInit(): void {
+    this.getMovies()
+    this.getFavorites()
+  }
+
+
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
+
+ 
+  openDescriptionView(Title: string, Description: string): void {
+    this.dialog.open(DescriptionViewComponent, {
+      data: { Title, Description }
+    })
+  }
+
+
+  openGenreView(Name: string, Description: string): void {
+    this.dialog.open(GenreViewComponent, {
+      data: { Name, Description }
+    })
+  }
+
+
+  openDirectorView(Name: string, Bio: string, Birth: string, Death: string): void {
+    this.dialog.open(DirectorViewComponent, {
+      data: { Name, Bio, Birth, Death }
+    })
+  }
+
+
+  getFavorites(): void {
+    const user = localStorage.getItem('user');
+    this.fetchApiData.getUser(user).subscribe((resp: any) => {
+      this.favorited = resp.FavoriteMovies;
+      console.log(this.favorited);
+      return this.favorited;
+    });
+  }
+
+
+  addFavMovie(movieId: string): void {
+    this.fetchApiData.addFavoriteMovies(movieId).subscribe((resp: any) => {
+      console.log(resp);
+      this.snackBar.open(`Added movie to Watchlist`, 'OK', {
+        duration: 4000,
+      });
+      this.getFavorites()
+      this.ngOnInit();
+    });
+  }
+
+
+  removeFavMovie(movieId: string): void {
+    this.fetchApiData.deleteMovie(movieId).subscribe((resp: any) => {
+      console.log(resp);
+      this.snackBar.open(`Removed movie from Watchlist`, 'OK', {
+        duration: 4000,
+      });
+      this.getFavorites()
+      this.ngOnInit();
+    });
+  }
+
+  
+  inFavorited(movieId: string): boolean {
+    if (this.favorited.includes(movieId)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
